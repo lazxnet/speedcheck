009f1e6f72f49ba68a99ef6ca9f4334f005fd996
Lázaro Campos <lazaromanuel65@gmail.com>
2025-01-28 13:12:01 -0500

    Update SpeedTest.jsx

--- src/SpeedTest.jsx
+++ src/SpeedTest.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion} from "framer-motion";
 
 // SVG icons
 const UploadIcon = () => (
